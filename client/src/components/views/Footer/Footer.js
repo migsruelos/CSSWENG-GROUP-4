c961b2f4effe47df5aa57bee3328516bab7a768a
miguelflores-0906 <miggy_4th@yahoo.com>
2021-08-16 23:51:39 +0800

    start on footer

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import {Icon} from 'antd';
+import './Sections/Footer.css';
 
 function Footer() {
     return (
-        <div style={{
-            height: '80px', display: 'flex',
-            flexDirection: 'column', alignItems: 'center',
-            justifyContent: 'center', fontSize:'1rem'
-        }}>
+        <div className="footer-wrapper">
            <p> Happy Coding  <Icon type="smile" /></p>
         </div> 
     )
---
+++ client/src/components/views/Footer/Sections/Blogpost.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Blogpost() {
+    return(
+        <div>
+            <h2>Latest Blog Post</h2>
+            <h1>Ready to get started?</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+    )
+}
+
+export default Blogpost;
