131d4d9aca78a2068f276c5e44b35e5775a5fa8b
migsruelos <87805608+migsruelos@users.noreply.github.com>
2021-08-26 23:31:05 +0800

    Updated mail function

--- client/src/components/views/ContactPage/Sections/SecondSection.js
+++ client/src/components/views/ContactPage/Sections/SecondSection.js
@@ -1,7 +1,38 @@
 import React from 'react'
 import './Contact.css'
+import { useState } from 'react';
+import { send } from 'emailjs-com';
+import{ init } from 'emailjs-com';
+init("user_DwlB71iH2cqNwLXXkLAGH");
 
 const SecondSection = () => {
+    const [toSend, setToSend] = useState({
+        from_name: '',
+        to_name: '',
+        message: '',
+        reply_to: '',
+      });
+    
+      const onSubmit = (e) => {
+        e.preventDefault();
+        send(
+          'service_7ra2jom',
+          'template_h74fncn',
+          toSend,
+          'user_DwlB71iH2cqNwLXXkLAGH'
+    )
+           .then((response) => {
+             console.log('SUCCESS!', response.status, response.text);
+           })
+           .catch((err) => {
+             console.log('FAILED...', err);
+           });
+      };
+    
+      const handleChange = (e) => {
+        setToSend({ ...toSend, [e.target.name]: e.target.value });
+      };
+
     return (
         <div className='second-contact'>
 
@@ -13,11 +44,12 @@ const SecondSection = () => {
             </div>
 
             <div className='second-contact-right'>
-                <form id='contactForm'>
-                    <input placeholder='Name'/> <br/>
-                    <input placeholder='Email'/> <br/>
-                    <textarea placeholder='Put your message here...' rows='9' cols='80'/> <br/>
-                    <button type='submit' className='form-button'>SEND</button>
+                <form id='contactForm' onSubmit={onSubmit}>
+                    <input type ='text' name='from_name' placeholder='from name' value={toSend.from_name} onChange={handleChange}/> <br/>
+                    <input type ='text' name='to_name' placeholder='to name' value={toSend.to_name} onChange={handleChange}/> <br/>
+                    <textarea type ='text' name='message' placeholder='message' rows='9' cols='80' value={toSend.message} onChange={handleChange}/> <br/>
+                    <input type ='text' name='reply_to' placeholder='your email' value={toSend.reply_to} onChange={handleChange}/><br/>
+                    <button type='submit' className='form-button' style={{marginTop: 30}}>Submit</button>
                 </form>
             </div>
             
